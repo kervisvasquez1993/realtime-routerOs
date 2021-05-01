dd9e8b0a814cd22e89cc11e3c0c22975742d7b54
kervis vasquez <kervisvasquez24@gmail.com>
2021-05-01 14:18:55 -0400

    ✨ develop add model and controller of UserMikrotik and infoMikrotik in proyect

---
+++ app/Http/Controllers/UserMikrotikController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\UserMikrotik;
+use Illuminate\Http\Request;
+
+class UserMikrotikController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\UserMikrotik  $userMikrotik
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserMikrotik $userMikrotik)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\UserMikrotik  $userMikrotik
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserMikrotik $userMikrotik)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\UserMikrotik  $userMikrotik
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserMikrotik $userMikrotik)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\UserMikrotik  $userMikrotik
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserMikrotik $userMikrotik)
+    {
+        //
+    }
+}
