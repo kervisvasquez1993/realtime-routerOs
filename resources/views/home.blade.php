ececef5a331bdfc2a19ac722874db45def14c364
kervis vasquez <kervisvasquez24@gmail.com>
2021-05-04 11:10:26 -0400

    :building_construction: develop agregamos variables para mostrar en tiempo real

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,96 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row">
+        <div class="col-md-6">
+            <h5 class="py-3 px-2">Información En tiempo Real de la interfaz <span id="interfazName"></span></h5>
+            <div class="card">
+                <div class="card-header"></div>                        
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item d-flex justify-content-between">
+                           <div>
+                            <h5 class="d-flex justify-content-between">
+                                <span class="font-weight-bold"> rx-byte: <p id="rxByte"></p></span> 
+                               
+                            </h5>
+    
+                           </div>                        
+                        </li>       
+                        <li class="list-group-item d-flex justify-content-between">
+                             <div>
+                                <h5 class="d-flex justify-content-between">
+                                    <span class="font-weight-bold"> tx-byte: <p id="txByte"></p> </span> 
+                                </h5>    
+                             </div>                        
+                        </li>     
+                       <li class="list-group-item d-flex justify-content-between">
+                            <div>
+                                <h5 class="d-flex justify-content-between">
+                                    <span class="font-weight-bold"> rx-packet : <p id="rxPacket"></p></span>   
+                                </h5>
+                            </div>                        
+                       </li>   
+                       <li class="list-group-item d-flex justify-content-between">
+                            <div>
+                                <h5 class="d-flex justify-content-between">
+                                    <span class="font-weight-bold"> tx-packet : <p id="txPacket"></p></span>   
+                                </h5>                            
+                            </div>                        
+                       </li> 
+                       
+                       <li class="list-group-item d-flex justify-content-between">
+                        <div>
+                            <h5 class="d-flex justify-content-between">
+                                <span class="font-weight-bold"> tx-queue-drop : <p id="txQqueueDrop"></p> </span>  
+                            </h5>
+                        </div>                        
+                       </li> 
+                       <li class="list-group-item d-flex justify-content-between">
+                        <div>
+                            <h5 class="d-flex justify-content-between">
+                                <span class="font-weight-bold"> fp-rx-byte : <p id="fpRxByte"></p> </span>   
+                            </h5>
+                        </div>                        
+                       </li> 
+                       <li class="list-group-item d-flex justify-content-between">
+                        <div>
+                            <h5 class="d-flex justify-content-between">
+                                <span class="font-weight-bold"> fp-tx-byte : <p id=""></p> </span> 
+                               
+                            </h5>
+                            
+                        </div>                        
+                       </li> 
+       
+                       <li class="list-group-item d-flex justify-content-between">
+                        <div>
+                            <h5 class="d-flex justify-content-between">
+                                <span class="font-weight-bold"> fp-rx-packet : <p id="fpRxPacket"></p> </span> 
+                               
+                                
+                            </h5>
+                            
+                        </div>                        
+                       </li> 
+                       <li class="list-group-item d-flex justify-content-between">
+                        <div>
+                            <h5 class="d-flex justify-content-between">
+                                <span class="font-weight-bold"> fp-tx-packet : <p id=""></p> </span>     
+                            </h5>
+                            
+                        </div>                        
+                       </li>     
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header d-flex justify-content-between">
                     <h4>Unefa</h4>
-
                     <a href="{{route('usermikrotiks.create')}}" class="btn btn-sm btn-secondary">Agregar Nuevo Usuario Mikrotik</a>
                 </div>
-
                 <div class="card-body">
                     @if (session('flash'))
                         <div class="alert alert-success" role="alert">
@@ -37,26 +118,48 @@
                 </div>
             </div>
         </div>
-    </div>
+          
+   </div>
 </div>
+
+
 @endsection
 
 @push('scripts')
     <script>
+        let nombrePuerto   = document.getElementById('interfazName'),
+            defaultName    = document.getElementById('defaultName'),
+            fpRxByte       = document.getElementById('fpRxByte'),/*  */
+            fpRxPacket     = document.getElementById('fpRxPacket'), /*  */
+            fpTxByte       = document.getElementById('fpTxByte'),
+            fpTxPacket     = document.getElementById('fpTxPacket'),
+            lastLinkUpTime = document.getElementById('lastLinkUpTime'),
+            macAddress     = document.getElementById('macAddress'),
+            rxByte         = document.getElementById('rxByte'), /*  */
+            rxPacket       = document.getElementById('rxPacket'),/*  */
+            txByte         = document.getElementById('txByte'), /*  */
+            txPacket       = document.getElementById('txPacket'), /*  */
+            type           = document.getElementById('type'),
+            txQqueueDrop   = document.getElementById('txQqueueDrop') /*  */
+
+
+
+
+
+
+
+
           Echo.channel('routerApi')
          .listen('RouterOs', (e) => {
-            console.log(e)
-            /* timerElement.innerText = e.time;
+            
+              
+            
 
-            circleElement.classList.add('refresh');
+            
 
-            winnerElement.classList.add('d-none');
-
-            resultElement.innerText = '';
-            resultElement.classList.remove('text-success');
-            resultElement.classList.remove('text-danger'); */
+           
         })
       
 </script>
-    </script>
+    
 @endpush
